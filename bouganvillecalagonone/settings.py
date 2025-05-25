b76b414a8a3164f0cc9a14dfff27a7971f477553
damusss <ricciardi.damiano06@gmail.com>
2025-05-25 17:59:54 +0200

    Change settings

--- bouganvillecalagonone/settings.py
+++ bouganvillecalagonone/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3*eqparwve$2+wfau0o)e%3r#7^9^n4xy7welhkqnmc^%-3nd8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app']
 
 
 # Application definition
