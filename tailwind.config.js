4eeb3be975b0d5db9e733f07a83ad7bf73ea842e
damusss <ricciardi.damiano06@gmail.com>
2026-05-10 01:55:35 +0200

    Drop tailwind CDN

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./sito/templates/**/*.html",
+    "./sito/static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
